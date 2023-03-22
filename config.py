2e1718f2d103af79f99341ed03b4fb0e163e13b3
michalszostak <michal.szostak56@gmail.com>
2023-03-22 22:31:34 +0000

    new file:   Procfile 	modified:   config.py 	modified:   requirements.txt

--- config.py
+++ config.py
@@ -1,20 +1,17 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-goes-here'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
+    SECRET_KEY = 'some-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    REMEMBER_COOKIE_DURATION = int(os.environ.get('REMEMBER_COOKIE_DURATION')) or 3600  # 1 hour in seconds
+    REMEMBER_COOKIE_DURATION = 3600
 
     # Email configuration
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 465)
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') == 'True' or True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'justq.main@gmail.com'
+    MAIL_PASSWORD = 'aqijdaltmxygxane'
 
     # URL for front-end consumer
-    FRONT_END_BASE_URL = os.environ.get('FRONT_END_BASE_URL')
+    FRONT_END_BASE_URL = 'https://sho-stack.github.io/justQ-react-frontend/'
