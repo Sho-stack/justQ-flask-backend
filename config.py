91fefc5e0d26f44b581894b40ed4e14fae652840
michalszostak <michal.szostak56@gmail.com>
2023-03-24 12:26:28 +0000

    updated config.py

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ class Config:
     MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
     MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
     MYSQL_HOST = os.environ.get("MYSQL_HOST")
-    MYSQL_PORT = os.environ.get("MYSQL_PORT")
     SECRET_KEY = os.environ.get("SECRET_KEY")
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
@@ -23,7 +22,7 @@ class Config:
     print("MAIL_PASSWORD:", MAIL_PASSWORD)
     
     # Set SQLALCHEMY_DATABASE_URI to use the MySQL configuration
-    SQLALCHEMY_DATABASE_URI = f'mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'
+    SQLALCHEMY_DATABASE_URI = f'mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REMEMBER_COOKIE_DURATION = 3600
