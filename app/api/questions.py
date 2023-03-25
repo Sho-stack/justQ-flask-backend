03ba9b29147bd8dbb63ceae80520d259a3fd14b6
michalszostak <michal.szostak56@gmail.com>
2023-03-25 19:38:37 +0000

    added route for adding replies - hotfix

--- app/api/questions.py
+++ app/api/questions.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from app.models import Question, User, Vote
+from app.models import Question, Answer, User, Vote
 from app import db
 from flask_login import login_required, current_user
 from datetime import datetime
