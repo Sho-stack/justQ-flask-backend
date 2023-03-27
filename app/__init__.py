18cbacfad0fd8fe1babda0f37c02031b82a567c4
MichalSzostak <michal.szostak56@gmail.com>
2023-03-27 12:20:05 +0100

    cookies fix 3

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    CORS(app, origins="*", supports_credentials=True)
+    CORS(app, origins=["http://localhost:3000", "https://justq-react.herokuapp.com"], supports_credentials=True)
     
     db.init_app(app)
     login_manager.init_app(app)
