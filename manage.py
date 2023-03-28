eba5cc917d4f97de44d4276e3f3c3fbe640ef139
michalszostak <michal.szostak56@gmail.com>
2023-03-28 01:01:32 +0100

    translation test 2 - hotfix 3

--- manage.py
+++ manage.py
@@ -1,33 +1,16 @@
 import os
 from app import create_app, db
-from flask_migrate import Migrate, init, migrate, upgrade
-import sys
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from config import Config
 
 app = create_app()
 app.config.from_object(Config)
 
-def init_migrate_upgrade():
-    with app.app_context():
-        migrate_instance = Migrate(app, db)
-        
-        if not os.path.exists("migrations"):
-            print("Initializing the migration repository...")
-            migrate_instance.init(directory="migrations")
-            print("Migration repository initialized.")
-        else:
-            print("Migration repository already exists.")
-        
-        print("Creating a new migration script...")
-        migrate_instance.migrate(directory="migrations")
+migrate = Migrate(app, db)
+manager = Manager(app)
 
-        print("Applying the migration script...")
-        migrate_instance.upgrade(directory="migrations")
-        print("Success.")
-
+manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "init_migrate_upgrade":
-        init_migrate_upgrade()
-    else:
-        print("Usage: python manage.py init_migrate_upgrade")
+    manager.run()
