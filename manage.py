e20cb322253dec1728512fbb61c28cbb16a31102
michalszostak <michal.szostak56@gmail.com>
2023-03-28 01:06:44 +0100

    translation test 2 - hotfix 4

--- manage.py
+++ manage.py
@@ -1,16 +1,16 @@
 import os
 from app import create_app, db
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 from flask_script import Manager
 from config import Config
 
 app = create_app()
 app.config.from_object(Config)
 
-migrate = Migrate(app, db)
 manager = Manager(app)
-
+migrate_instance = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+
 
 if __name__ == "__main__":
     manager.run()
