c4009f354040b4fb9fa656a91dc88e26d74e1832
MichalSzostak <michal.szostak56@gmail.com>
2023-03-29 16:54:23 +0100

    added sorting parameter for get quesitons

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import URLSafeTimedSerializer
 from flask import current_app
-
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.sql.expression import case
 
 
